313404fd57aac126286be22b40fbd94bb31d1b77
Ifedapo Abdullahi Salami <abdusalife@gmail.com>
2020-12-29 12:07:20 +0800

    Turbine Flow Control

---
+++ Turbine Flow Control.ino
@@ -0,0 +1,14 @@
+int motorPin = 9;
+void setup(){
+ pinMode(motorPin, OUTPUT);
+}
+void loop() {
+ for(int motorValue = 0 ; motorValue <= 255; motorValue +=5){
+ analogWrite(motorPin, motorValue);
+ delay(30);
+ }
+ for(int motorValue = 255 ; motorValue >= 0; motorValue -=5){
+ analogWrite(motorPin, motorValue);
+ delay(30);
+ }
+}
